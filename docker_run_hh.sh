4baba310aa1929ec4b2c35990c1c077693d78396
Nethika Suraweera <nethika@gmail.com>
2020-09-05 23:21:00 -0600

    Update docker_run_hh.sh

--- docker_run_hh.sh
+++ docker_run_hh.sh
@@ -1,2 +1,2 @@
 docker rm -f hh_app
-nvidia-docker run --rm --name hh_app -ti -p 8000:8000 -v /cloud/data/hh_credit_card_app:/srv/hh_credit_card_app tinmankinetics/tensorflow:latest /bin/bash 
+nvidia-docker run --rm --name hh_app -ti -p 8000:8000 -v /cloud/data/hh_credit_card_app:/srv/hh_credit_card_app ns/tensorflow:latest /bin/bash 
